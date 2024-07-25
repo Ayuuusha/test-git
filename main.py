f76e018a03c69752ce283a39cef5fc110bcf22df
Ayuuusha <aisatoktobolotova@gmail.com>
2024-07-25 17:34:03 +0500

    add, updated main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print('''Stray Kids, они же SKZ, они же «те, что не BTS» — 
        в 2017 году. Группа состоит из восьми обаятельных, талантливых 
       и целеустремленных молодых людей, грезивших о всемирной 
       славе и на ура исполнивших эту мечту.''')
+
+
+name = 'Felix'
+print('Favorite member ' + name)
+
+name1 = input('Enter name: ')
+print('Your favorite member: ' + name1)
